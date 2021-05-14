2ed04a0e17153ff43c219bad4a133096f7580ef6
dekunleroy <adebanjosegun589@gmail.com>
2021-05-14 22:25:51 +0100

    update headers

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true, limit: '5mb ' }));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*', 'Access-Control-Allow-Headers', '*');
+  res.header({ 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Headers': '*' });
   next();
 });
 
